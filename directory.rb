6216a61b595ae6a9ed71aea1d8eaed88a710df83
LUNDI19 <michaelmcloughlin9@gmail.com>
2021-05-17 17:58:29 +0200

    changed to .each method and .count method

--- directory.rb
+++ directory.rb
@@ -1,16 +1,21 @@
-puts "The students of Villians Academy"
-puts "------------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex Delarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-print "Overall we have"
-print "11"
-puts "great students"
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader", 
+  "Nurse Ratched", 
+  "Michael Corleone", 
+  "Alex Delarge", 
+  "The Wicked Witch of the West", 
+  "Terminator",
+  "Freddy Krueger", 
+  "The Joker", 
+  "Joffrey Baratheon", 
+  "Norman Bates",
+]
+
+puts "The students of villains Academy"
+puts "---------------------"
+students.each do |student|
+  puts student
+end
+
+puts "Overall, we have #{students.count} great students"
